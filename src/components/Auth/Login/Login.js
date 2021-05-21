b8fc35685b847cad36784596a5f3a164cdcf4487
dvkin <dennis.kinyaev@gmail.com>
2021-05-21 21:17:07 +0300

    login and register button fixed

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,72 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import { useHistory } from 'react-router';
+import { useHistory } from "react-router";
 
-import Button from '../Button/Button';
-import Fieldset from '../Fieldset/Fieldset';
-import Form from '../Form/Form';
-import Input from '../Input/Input';
-import Link from '../Link/Link';
-import Text from '../Text/Text';
+import Button from "../Button/Button";
+import Fieldset from "../Fieldset/Fieldset";
+import Form from "../Form/Form";
+import Input from "../Input/Input";
+import Link from "../Link/Link";
+import Text from "../Text/Text";
 import AuthWithForm from "../AuthWithForm";
+
 function Login({ onLogin, onError, onClearMessages }) {
-    const history = useHistory();
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  //   const history = useHistory();
+  //   const [email, setEmail] = React.useState("");
+  //   const [password, setPassword] = React.useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onLogin(email, password);
-        //history.push('/movies');
-    };
-    const { errors, isValid, handleChange } = AuthWithForm();
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Fieldset>
-                <Input
-                    title="E-mail"
-                    placeholder="E-mail"
-                    id="email"
-                    type="email"
-                    required
-                    isError={errors.email}
-                    onFocus={onClearMessages}
-                    value={email}
-                    onChange={(evt) => {
-                        setEmail(evt.target.value);
-                    }}
-                />
-                <Input
-                    title="Пароль"
-                    placeholder="Пароль"
-                    type="password"
-                    id="password"
-                    isError={errors.password}
-                    onError={onError}
-                    onFocus={onClearMessages}
-                    value={password}
-                    onChange={(evt) => {
-                        setPassword(evt.target.value);
-                    }}
-                    required
-                />
-            </Fieldset>
-            <div>
-                <Button>
-                    Войти
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+    //history.push('/movies');
+  };
+  const { password, name, email, errors, isValid, handleChange } =
+    AuthWithForm();
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Fieldset>
+        <Input
+          title="E-mail"
+          placeholder="E-mail"
+          id="email"
+          type="email"
+          name="email"
+          required
+          isError={errors.email}
+          onFocus={onClearMessages}
+          value={email}
+          onChange={handleChange}
+        />
+        <Input
+          title="Пароль"
+          placeholder="Пароль"
+          type="password"
+          id="password"
+          name="password"
+          isError={errors.password}
+          onError={onError}
+          onFocus={onClearMessages}
+          value={password}
+          onChange={handleChange}
+          required
+        />
+      </Fieldset>
+      <div>
+        <Button isActive={isValid} type="submit">
+          Войти
         </Button>
-                <Text>
-                    Ещё не зарегистрированы?
-          {' '}
-                    <Link
-                        to='/signup'
-                        name="Регистрация"
-                    />
-                </Text>
-            </div>
-        </Form>
-    );
-};
-
+        <Text>
+          Ещё не зарегистрированы? <Link to="/signup" name="Регистрация" />
+        </Text>
+      </div>
+    </Form>
+  );
+}
 
 export default Login;
